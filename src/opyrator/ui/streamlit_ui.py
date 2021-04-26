ce6699dd14b2f1e851bad0feaff6b96109fa8d1e
raethlein <benjamin.raethlein@gmail.com>
2021-04-26 12:22:36 +0200

    Fix linter issue

--- src/opyrator/ui/streamlit_ui.py
+++ src/opyrator/ui/streamlit_ui.py
@@ -48,7 +48,7 @@ def launch_ui(opyrator_path: str, port: int = 8501) -> None:
 
         python_path = f'PYTHONPATH="$PYTHONPATH:{getcwd()}"'
         if system() == 'Windows':
-            python_path = f"set PYTHONPATH=%PYTHONPATH%;{getcwd()} &&" 
+            python_path = f"set PYTHONPATH=%PYTHONPATH%;{getcwd()} &&"
 
         subprocess.run(
             f'{python_path} {sys.executable} -m streamlit run --server.port={port} --server.headless=True --runner.magicEnabled=False --server.maxUploadSize=50 --browser.gatherUsageStats=False {f.name}',
